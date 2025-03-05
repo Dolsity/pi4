23e3199a73c1819fd2c4a4a6620b67fea9edb9c5
Jay <Acorder43812@gmail.com>
2025-03-04 23:16:49 -0800

    Create app_info.py

---
+++ pi4/app_info.py
@@ -0,0 +1,11 @@
+import os
+
+__app_name__ = 'pi4'
+__version__ = '0.1.0' # MAJOR.MINOR.PATCH
+
+try:
+    username = os.getlogin()
+except:
+    username = os.popen("echo ${SUDO_USER:-$(who -m | awk '{ print $1 }')}").readline().strip()
+
+config_file = f'/opt/{__app_name__}/config.txt'
